88108317964f27cbd61d1bd581d29af246d22dd9
catherine genao <cgenao80@gmail.com>
2020-06-19 17:11:25 -0400

    play button scree2n

--- FinalProject/app/src/main/java/com/example/finalproject/LevelPage.java
+++ FinalProject/app/src/main/java/com/example/finalproject/LevelPage.java
@@ -1,4 +1,14 @@
 package com.example.finalproject;
 
-public class LevelPage {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LevelPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_page);
+    }
 }
