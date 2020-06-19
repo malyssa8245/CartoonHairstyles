e3e43b635dd39633101fe33d310c40e91550152a
catherine genao <cgenao80@gmail.com>
2020-06-19 16:44:11 -0400

    play button screen

--- FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,14 +2,40 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView playbutton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        playbutton.setImageResource(R.drawable.playbutton);
+
+       playbutton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               goToLevelPage();
+           }
+       });
     }
+
+public void goToLevelPage(){
+
+        Intent i = new Intent(this,)
+}
+
+
 }
 //this a testing comment catherine //
