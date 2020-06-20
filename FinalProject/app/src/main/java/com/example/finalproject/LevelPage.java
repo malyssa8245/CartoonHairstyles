b523d19c12cc15235bf3a5d2d519cedd7bd205b6
catherine genao <cgenao80@gmail.com>
2020-06-20 10:06:57 -0400

    NEW

--- FinalProject/app/src/main/java/com/example/finalproject/LevelPage.java
+++ FinalProject/app/src/main/java/com/example/finalproject/LevelPage.java
@@ -2,13 +2,40 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LevelPage extends AppCompatActivity {
+ImageView Atime;
+ImageView Tdrama;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_page);
+        Atime.setImageResource(R.drawable.time);
+        Tdrama.setImageResource(R.drawable.total);
+
+     Atime = findViewById(R.id.Atime);
+     Tdrama = findViewById(R.id.Tdrama);
+
+
+        Atime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToAdventureTimeQuiz();
+            }
+        });
     }
+
+    public void goToAdventureTimeQuiz(){
+
+        Intent i = new Intent(this,AdventureTimeQuiz.class);
+        startActivity(i);
+    }
+
+
 }
