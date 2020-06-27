61b3b64f599e652e0cf82899e93ee91d535ee169
catherine genao <cgenao80@gmail.com>
2020-06-26 20:03:26 -0400

    NEW

--- FinalProject/app/src/main/java/com/example/finalproject/AdventureTimeQuiz.java
+++ FinalProject/app/src/main/java/com/example/finalproject/AdventureTimeQuiz.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class AdventureTimeQuiz extends AppCompatActivity {
 
     TextView question;
@@ -14,8 +17,15 @@ public class AdventureTimeQuiz extends AppCompatActivity {
     Button a3;
     Button a4;
     Button a5;
+ArrayList <HashMap> questions;
+    int counter = 0;
+    Question q1;
+    Question q2;
+    Question q3;
+    Question q4;
+    Question q5;
 
-    @Override
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adventure_time_quiz);
@@ -26,21 +36,52 @@ public class AdventureTimeQuiz extends AppCompatActivity {
         a4 = findViewById(R.id.a4);
         a5 = findViewById(R.id.a5);
 
-        Question question1 = new Question("What do you want to be in the future?","Pediatrician", "Musician","The President","Hero", "Comedian" );
+        questions = new ArrayList<>();
+        HashMap<String, String> question1 = new HashMap<String, String>();
+        question1.put("question", "What do you want to be in the future?");
+        question1.put("Pediatrician", "Princess Bubblegum");
+        question1.put("Musician", "Marceline");
+        question1.put("The President", "the Ice king");
+        question1.put("Hero", "finn");
+        question1.put("Comedian", "jake");
+      questions.add(question1);
 
-        Question question2 = new Question(" What would people describe you as?","Responsible, Kind","Down to earth,chill","Uptight, Short tempered", "Adventurous, Helpful, Loving","funny, tough, loveable");
-        Question question3 = new Question("You go to the beach and your friend forgets sunscreen. What do you do?","I would most likely come with extra sunscreen.","Oh well, that’s not my problem.","I wasn’t invited so… ");
-       // Question question4 = new Question("How would you plan a party?");
-      //  Question question5 = new Question("If you knew you had a few days left to live, what would you do?");
 
-        question.setText(question1.getQuestion());
-        question.setText(question2.getQuestion());
+        HashMap<String, String> question2 = new HashMap<String, String>();
+        question2.put("question2", "What would people describe you as?");
+        question2.put("Responsible, Kind", "Princess Bubblegum");
+        question2.put("Down to earth,chill", "Marceline");
+        question2.put("Uptight, Short tempered", "the Ice king");
+        question2.put("Adventurous, Helpful, Loving", "finn");
+        question2.put("funny, tough, loveable", "jake");
+        questions.add(question2);
 
 
 
+
+
+
+    q1 = new Question(" What do you want to be in the future?","Pediatrician","Musician","The President", "Hero","Comedian");
+       q2 = new Question(" What would people describe you as?","Responsible, Kind","Down to earth,chill","Uptight, Short tempered", "Adventurous, Helpful, Loving","funny, tough, loveable");
+       q3 = new Question("You go to the beach and your friend forgets sunscreen. What do you do?","I would most likely come with extra sunscreen.","Oh well, that’s not my problem.","I wasn’t invited so… ");
+       // Question q4 = new Question("How would you plan a party?");
+      //  Question q5 = new Question("If you knew you had a few days left to live, what would you do?");
+
+
+       // question.setText(question2.getQuestion());
+
+nextquestion();
+
+
+    }
+
+
+    public void nextquestion(){
+
+
+        question.setText(questions.get(counter).get("question").toString());
 
     }
 
 }
 
-whe
